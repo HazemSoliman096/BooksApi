e770e9554b99d71f16751c952789a63c7ece42b8
Hazem Abd el-Aziz <hazemsoliman096@gmail.com>
2022-11-05 06:39:22 +0200

    add services and dbcontext in program.cs

--- BooksApi/Program.cs
+++ BooksApi/Program.cs
@@ -1,3 +1,8 @@
+using BooksApi.Data;
+using BooksApi.Repository.Interfaces;
+using BooksApi.Repository.Classes;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>( op
+    => op.UseNpgsql(builder.Configuration.GetConnectionString("SqlConnectionString")));
+
+builder.Services.AddScoped<IFormatRepository, FormatRepository>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 
 var app = builder.Build();
 
